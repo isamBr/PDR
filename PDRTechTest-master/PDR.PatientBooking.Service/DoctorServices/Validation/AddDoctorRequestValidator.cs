d6a58d1c526279cd3fe9d894f7a09b1c78e716eb
Isam Brahim <isam.brahim@mycit.ie>
2021-03-09 12:51:50 +0000

    solutions 12bugs

---
+++ PDRTechTest-master/PDR.PatientBooking.Service/DoctorServices/Validation/AddDoctorRequestValidator.cs
@@ -0,0 +1,134 @@
+﻿using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Service.DoctorServices.Requests;
+using PDR.PatientBooking.Service.Enums;
+using PDR.PatientBooking.Service.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+
+namespace PDR.PatientBooking.Service.DoctorServices.Validation
+{
+    public class AddDoctorRequestValidator : IAddDoctorRequestValidator
+    {
+        private readonly PatientBookingContext _context;
+
+        public AddDoctorRequestValidator(PatientBookingContext context)
+        {
+            _context = context;
+        }
+
+        public PdrValidationResult ValidateRequest(AddDoctorRequest request)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (MissingRequiredFields(request, ref result))
+                return result;
+
+            if (DoctorEmailValidation(request, ref result))
+                return result;
+
+            if (DoctorAlreadyInDb(request, ref result))
+                return result;
+
+            if (DoctorGenderValidation(request, ref result))
+                return result;
+
+            return result;
+        }
+
+        public bool MissingRequiredFields(AddDoctorRequest request, ref PdrValidationResult result)
+        {
+            var errors = new List<string>();
+
+            if (string.IsNullOrEmpty(request.FirstName))
+                errors.Add("FirstName must be populated");
+
+            if (string.IsNullOrEmpty(request.LastName))
+                errors.Add("LastName must be populated");
+
+            if (string.IsNullOrEmpty(request.Email))
+                errors.Add("Email must be populated");
+
+           
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DoctorAlreadyInDb(AddDoctorRequest request, ref PdrValidationResult result)
+        {
+            if (_context.Doctor.Any(x => x.Email == request.Email))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A doctor with that email address already exists");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DoctorGenderValidation(AddDoctorRequest request, ref PdrValidationResult result)
+        {
+            if (Enum.IsDefined(typeof(Gender), request.Gender))
+            {
+                return false;
+            }
+            else
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Gender value not valid  0:Female 1:Male 2:Other");
+                return true;
+            }
+        }
+
+        private bool DoctorEmailValidation(AddDoctorRequest request, ref PdrValidationResult result)
+        {
+
+            var email = request.Email;
+        
+         
+                if (email == "")
+                {
+                    result.PassedValidation = false;
+                    result.Errors.Add("Email must be a valid email address");
+                    return true;
+                }
+                else if (email == null)
+                {
+                    result.PassedValidation = false;
+                    result.Errors.Add("Email must be populated");
+                    return true;
+                }
+
+            
+
+            if (email=="")
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Email must be a valid email address");
+                return true;
+            }
+            try
+            {
+                MailAddress m = new MailAddress(email);
+
+                return false;
+            }
+            catch (FormatException)
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Email must be a valid email address");
+                return true;
+            }
+        }
+
+
+    }
+}
---
+++ PDRTechTest-master/PDR.PatientBooking.Service/PatientServices/Validation/AddPatientRequestValidator.cs
@@ -0,0 +1,121 @@
+﻿using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Service.PatientServices.Requests;
+using PDR.PatientBooking.Service.Validation;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace PDR.PatientBooking.Service.PatientServices.Validation
+{
+    public class AddPatientRequestValidator : IAddPatientRequestValidator
+    {
+        private readonly PatientBookingContext _context;
+
+        public AddPatientRequestValidator(PatientBookingContext context)
+        {
+            _context = context;
+        }
+
+        public PdrValidationResult ValidateRequest(AddPatientRequest request)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (MissingRequiredFields(request, ref result))
+                return result;
+     
+            if (PatientAlreadyInDb(request, ref result))
+                return result;
+
+            if (ClinicNotFound(request, ref result))
+                return result;
+
+            if (PatientEmailValidation(request, ref result))
+                return result;
+
+
+
+            return result;
+        }
+
+        private bool MissingRequiredFields(AddPatientRequest request, ref PdrValidationResult result)
+        {
+            var errors = new List<string>();
+
+            if (string.IsNullOrEmpty(request.FirstName))
+                errors.Add("FirstName must be populated");
+
+            if (string.IsNullOrEmpty(request.LastName))
+                errors.Add("LastName must be populated");
+
+            if (string.IsNullOrEmpty(request.Email))
+                errors.Add("Email must be populated");
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool PatientAlreadyInDb(AddPatientRequest request, ref PdrValidationResult result)
+        {
+            if (_context.Patient.Any(x => x.Email == request.Email))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A patient with that email address already exists");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool ClinicNotFound(AddPatientRequest request, ref PdrValidationResult result)
+        {
+            if (!_context.Clinic.Any(x => x.Id == request.ClinicId))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A clinic with that ID could not be found");
+                return true;
+            }
+
+            return false;
+        }
+
+         private bool PatientEmailValidation(AddPatientRequest request, ref PdrValidationResult result)
+        {
+          
+            var email = request.Email;
+            if (email == "")
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Email must be a valid email address");
+                return true;
+            }
+            else if (email == null)
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Email must be populated");
+                return true;
+            }
+            try
+            {
+                MailAddress m = new MailAddress(email);
+
+                return false;
+            }
+            catch (FormatException)
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("Email must be a valid email address");
+                return true;
+            }
+        }
+    
+    }
+}
